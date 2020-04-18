e389340396eda82ab723a915808291075b3a8ce8
InternetCupid <60466083+InternetCupid@users.noreply.github.com>
2020-04-18 17:08:50 +0200

    Update perso.c

--- RogueLike/src/perso.c
+++ RogueLike/src/perso.c
@@ -23,13 +23,12 @@
  */
 void PassageBossFinal() {
 	
-	if (sound != NULL) {
-		Mix_FreeMusic(sound);
-		sound = NULL;
+	if (music1 != NULL) {
+		Mix_FreeChunk(music1);
+		music1= NULL;
 	}
-
-	sound = Mix_LoadMUS("./sounds/boss.wav");
-	Mix_PlayMusic(sound, 1);
+	music1 = Mix_LoadWAV("./sounds/boss.wav");
+	Mix_PlayChannel(-1,music1, -1);
 }
 
 
@@ -63,13 +62,12 @@ void AttaquePlayer (SDL_Renderer * rendu, Player * player, Salle * salle, Salle 
 	for (int i = 0 ; i < salle->nbMonstres ; i++) {
 		if (((salle->monstre[i].salleX+salle->monstre[i].tailleX >= (player->salleX-player->tailleX-5)) && (salle->monstre[i].salleX <= (player->salleX+2*player->tailleX))) && ((salle->monstre[i].salleY+salle->monstre[i].tailleY >= (player->salleY-player->tailleY-5)) && (salle->monstre[i].salleY <= (player->salleY+2*player->tailleY)))){
 			if (!salle->monstre[i].bossFinal) {
-				if (sound != NULL) {
-					Mix_FreeMusic(sound);
-					sound = NULL;
+				if (music2 != NULL) {
+					Mix_FreeChunk(music2);
+					music2= NULL;
 				}
-
-				sound = Mix_LoadMUS("./sounds/hit.wav");
-				Mix_PlayMusic(sound, 1);
+				music2 = Mix_LoadWAV("./sounds/hit.wav");
+				Mix_PlayChannel(-1,music2, 0);
 			}
 			salle->monstre[i].hp -= player->damage;
 			if (salle->monstre[i].hp <= 0) {
@@ -79,10 +77,12 @@ void AttaquePlayer (SDL_Renderer * rendu, Player * player, Salle * salle, Salle 
 					salle->coffre[salle->nbCoffre].salleY = salle->monstre[i].salleY + (salle->monstre[i].tailleY/2);
 					salle->coffre[salle->nbCoffre].ouvert = 0;
 					salle->nbCoffre++;
-					if (sound != NULL) {
-						Mix_FreeMusic(sound);
-						sound = NULL;
+					if (music1!= NULL) {
+						Mix_FreeChunk(music1);
+						music1 = NULL;
 					}
+					music1 = Mix_LoadWAV("./sounds/donjon_salle.wav");
+					Mix_PlayChannel(-1,music1, -1);
 				}
 				LibererAnimation(salle->monstre[i].An);
 				LibererSprite(salle->monstre[i].sp);
@@ -248,12 +248,12 @@ int ActionSalle(Input * in, SDL_Renderer * rendu, TTF_Font * police, Player * pl
 				}
 				else {
 					InitObjet(&player->damage, &player->def);
-					if (sound != NULL) {
-						Mix_FreeMusic(sound);
-						sound = NULL;
+					if (music2 != NULL) {
+					Mix_FreeChunk(music2);
+					music2= NULL;
 					}
-					sound = Mix_LoadMUS("./sounds/GetCoffre.wav");
-					Mix_PlayMusic(sound, 1);
+					music2 = Mix_LoadWAV("./sounds/GetCoffre.wav");
+					Mix_PlayChannel(-1,music2, 0);
 				}
 			}
 		}
