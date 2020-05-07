2b802eb4abdce6319d9cefb57c41f1328d5f6c09
InternetCupid <60466083+InternetCupid@users.noreply.github.com>
2020-05-07 09:23:25 +0200

    Update roguelike.c

--- RogueLike/src/roguelike.c
+++ RogueLike/src/roguelike.c
@@ -79,7 +79,6 @@ int main(int argc, char ** argv) {
 
 	Labyrinthe labyrinthe;
 
-	sound = NULL;
 	VOLUME = MIX_MAX_VOLUME/2;
 	ChargerSon();
 
@@ -90,13 +89,12 @@ int main(int argc, char ** argv) {
 	while (!in.quit) {
 
 		//On va lancer la musique du menu en s'assurant de stoper les sons deja present sur le canal
-		if (sound != NULL) {
-			Mix_FreeMusic(sound);
-			sound = NULL;
+		if (background != NULL) {
+			Mix_FreeChunk(background);
+			background= NULL;
 		}
-
-		sound = Mix_LoadMUS("./sounds/mainpage.wav");
-		Mix_PlayMusic(sound, 1);
+		background = Mix_LoadWAV("./sounds/mainpage.wav");
+		Mix_PlayChannel(-1,background, -1);
 		Mix_VolumeMusic(VOLUME);
 
 		//Durant que le joueur est dans les menus principaux du jeu
@@ -182,11 +180,13 @@ int main(int argc, char ** argv) {
 			SDL_Delay(2);
 		}
 
-		//On va eteindre la musique du menu si elle est encore lancer
-		if (sound != NULL) {
-			Mix_FreeMusic(sound);
-			sound = NULL;
+		//On va eteindre la musique du menu si on entre le jeu et elle est encore lancer
+		if (background != NULL) {
+			Mix_FreeChunk(background);
+			background= NULL;
 		}
+		background = Mix_LoadWAV("./sounds/donjon_salle.wav");
+		Mix_PlayChannel(-1,background, -1);
 
 		//Durant que le joueur est en partie
 		while (inGame && !in.quit) {
@@ -255,9 +255,12 @@ int main(int argc, char ** argv) {
 		LibererPlayer(&player);
 		LibererMonstres(salle);
 	}
-
-	if (sound != NULL)
-		Mix_FreeMusic(sound);
+	if (background != NULL){
+		Mix_FreeChunk(background);
+	}
+	if (action != NULL){
+		Mix_FreeChunk(action);
+	}
 
 	Mix_CloseAudio();
 	TTF_Quit();
